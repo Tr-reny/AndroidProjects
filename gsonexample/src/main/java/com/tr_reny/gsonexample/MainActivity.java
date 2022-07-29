771ae0d6fdb1350a4682d28b5e28ccff66a376eb
Tr-reny <rennykipkoech@gmail.com>
2022-07-29 15:54:14 -0400

    from json to javaobject

--- gsonexample/src/main/java/com/tr_reny/gsonexample/MainActivity.java
+++ gsonexample/src/main/java/com/tr_reny/gsonexample/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Gson gson = new Gson();
+
+    /*    Employee employee = new Employee("john", 30,"john@gmail.com");
+        String json = gson.toJson(employee);*/
+
+        //creating Json in String Form
+        String json = "{\"firstname\":\"John\",\"age\":30,\"mail\":\"john@gmail.com\"}";
+        Employee employee = gson.fromJson(json, Employee.class);
     }
 }
