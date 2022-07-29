2cff3914234ac02e529939516a6e04bd3510c8b3
Tr-reny <rennykipkoech@gmail.com>
2022-07-29 18:24:09 -0400

    realtime database

---
+++ realtimerecyleview/src/main/java/com/tr_reny/realtimerecyleview/user.java
@@ -0,0 +1,17 @@
+package com.tr_reny.realtimerecyleview;
+
+public class user {
+    String firstName, lastName, age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
