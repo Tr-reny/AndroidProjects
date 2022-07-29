8e292414e63c0ef470b1f384a0f40f12984ffdee
Tr-reny <rennykipkoech@gmail.com>
2022-07-29 15:11:46 -0400

    Employee.java

---
+++ gsonexample/src/main/java/com/tr_reny/gsonexample/Employee.java
@@ -0,0 +1,14 @@
+package com.tr_reny.gsonexample;
+
+public class Employee {
+    private String firstName;
+    private int age;
+    private  String mail;
+
+    public Employee (String firstName, int age, String mail){
+        this.firstName = firstName;
+        this.age= age;
+        this.mail = mail;
+
+    }
+}
