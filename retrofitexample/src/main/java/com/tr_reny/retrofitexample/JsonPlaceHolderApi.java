124615f9bd2c79bde58dd6d2069f769be70743cf
Tr-reny <rennykipkoech@gmail.com>
2022-07-31 14:58:34 -0400

    json place holder

--- retrofitexample/src/main/java/com/tr_reny/retrofitexample/JsonPlaceHolderApi.java
+++ retrofitexample/src/main/java/com/tr_reny/retrofitexample/JsonPlaceHolderApi.java
@@ -1,4 +1,12 @@
 package com.tr_reny.retrofitexample;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPost();
 }
--- retrofitexample/src/main/java/com/tr_reny/retrofitexample/MainActivity.java
+++ retrofitexample/src/main/java/com/tr_reny/retrofitexample/MainActivity.java
@@ -3,13 +3,53 @@ package com.tr_reny.retrofitexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewResults = findViewById(R.id.txtPost);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        Call<List<Post>> call = jsonPlaceHolderApi.getPost();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                if (!response.isSuccessful()){
+                    textViewResults.setText("Code :" + response.code());
+                    return;
+                }
+
+                List<Post> posts = response.body();
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResults.setText(t.getMessage());
+
+            }
+        });
 
     }
 }
