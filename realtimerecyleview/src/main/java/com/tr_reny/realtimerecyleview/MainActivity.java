e42800b34e369d7bc50f45d83d4db66ac84723fd
Tr-reny <rennykipkoech@gmail.com>
2022-07-31 08:15:43 -0400

    minor updates

--- realtimerecyleview/src/main/java/com/tr_reny/realtimerecyleview/MainActivity.java
+++ realtimerecyleview/src/main/java/com/tr_reny/realtimerecyleview/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
  public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +15,13 @@ import android.os.Handler;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
+        Button test = findViewById(R.id.btnTest);
+        test.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                Intent splash = new Intent(MainActivity.this,userlist.class);
-                startActivity(splash);
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,userlist.class);
+                startActivity(intent);
             }
-        },2*1000);
+        });
     }
 }
