5219918656d808e54e4986288ac91d955d14bb86
Tr-reny <rennykipkoech@gmail.com>
2022-08-01 02:40:37 -0400

    created Marvel

---
+++ revisionretrofitexample/src/main/java/com/tr_reny/revisionretrofitexample/marvel.java
@@ -0,0 +1,24 @@
+package com.tr_reny.revisionretrofitexample;
+
+public class marvel {
+    private String name;
+    private String realname;
+    private String team;
+    private String firstappearance;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public String getFirstappearance() {
+        return firstappearance;
+    }
+}
