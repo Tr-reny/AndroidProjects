c2e1b1154b81855041df1c5b545fd6ed2d1228f1
Tr-reny <rennykipkoech@gmail.com>
2022-08-04 04:41:22 -0400

    created API

--- retrofitmovie/src/main/java/com/tr_reny/retrofitmovie/MainActivity.java
+++ retrofitmovie/src/main/java/com/tr_reny/retrofitmovie/MainActivity.java
@@ -20,6 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        textViewResults = findViewById(R.id.textMarvelsResults);
     }
 }
