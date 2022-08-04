09e4c2f1c684bb7253bf3b2f9484aedced53bfb2
Tr-reny <rennykipkoech@gmail.com>
2022-08-04 15:45:56 -0400

    added api

---
+++ advanceapi/src/main/java/com/tr_reny/advanceapi/Config.java
@@ -0,0 +1,6 @@
+package com.tr_reny.advanceapi;
+
+public class Config {
+    public static final String unsplash_access_key = "cr7PWUY1609mfaQ7OWgXzC5RKVLjuQ2JAPEahjXMzb0";
+    public static final String BASE_URL_UNSPLASH = "https://api.unsplash.com/";
+}
