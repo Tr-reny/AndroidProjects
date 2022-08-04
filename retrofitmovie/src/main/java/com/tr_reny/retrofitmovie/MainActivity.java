399772d9bc2e76626d4a8aa53d23680a0569e971
Tr-reny <rennykipkoech@gmail.com>
2022-08-04 05:12:02 -0400

    append

--- retrofitmovie/src/main/java/com/tr_reny/retrofitmovie/MainActivity.java
+++ retrofitmovie/src/main/java/com/tr_reny/retrofitmovie/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -21,5 +23,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textViewResults = findViewById(R.id.textMarvelsResults);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.simplifiedcoding.net/demos/marvel/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        SimplifiedCodingAPI simplifiedCodingAPI = retrofit.create(SimplifiedCodingAPI.class);
+
+        Call<List<Marvel>> call = simplifiedCodingAPI.getMarvels();
+
+        call.enqueue(new Callback<List<Marvel>>() {
+            @Override
+            public void onResponse(Call<List<Marvel>> call, Response<List<Marvel>> response) {
+                if (!response.isSuccessful()){
+                    textViewResults.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Marvel> marvels = response.body();
+                for (Marvel marvel : marvels){
+                    String content = "";
+                    content += "Name: " + marvel.getName() + "\n";
+                    content += "RealName: " + marvel.getRealname() + "\n";
+                    content += "Team: " + marvel.getTeam() + "\n";
+                    content += "Bio: " + marvel.getBio() + "\n";
+                    content += "ImageUrl: " + marvel.getImageurl() + "\n\n";
+
+                    textViewResults.append(content);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Marvel>> call, Throwable t) {
+              textViewResults.setText(t.getMessage());
+
+            }
+        });
+
     }
 }
--- retrofitmovie/src/main/java/com/tr_reny/retrofitmovie/SimplifiedCodingAPI.java
+++ retrofitmovie/src/main/java/com/tr_reny/retrofitmovie/SimplifiedCodingAPI.java
@@ -8,7 +8,7 @@ import retrofit2.http.GET;
 public interface SimplifiedCodingAPI {
 
     @GET("marvel")
-    Call<List<Marvel>> getMarvel();
+    Call<List<Marvel>> getMarvels();
 
 
 }
