e399a53be34142e5ae850192e85748ea855dbcdc
Tr-reny <rennykipkoech@gmail.com>
2022-08-05 11:21:19 -0400

    horizontal recycleView

---
+++ horizontalrecyclerview/src/main/java/com/tr_reny/horizontalrecyclerview/MainActivity.java
@@ -0,0 +1,83 @@
+package com.tr_reny.horizontalrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Recycler View object
+    RecyclerView recyclerView;
+
+    // Array list for recycler view data source
+    ArrayList<String> source;
+
+    // Layout Manager
+    RecyclerView.LayoutManager RecyclerViewLayoutManager;
+
+    // adapter class object
+    Adapter adapter;
+
+    // Linear Layout Manager
+    LinearLayoutManager HorizontalLayout;
+
+    View ChildView;
+    int RecyclerViewItemPosition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialisation with id's
+        recyclerView
+                = (RecyclerView)findViewById(
+                R.id.recyclerview);
+        RecyclerViewLayoutManager
+                = new LinearLayoutManager(
+                getApplicationContext());
+
+        // Set LayoutManager on Recycler View
+        recyclerView.setLayoutManager(
+                RecyclerViewLayoutManager);
+
+        // Adding items to RecyclerView.
+        AddItemsToRecyclerViewArrayList();
+
+        // calling constructor of adapter
+        // with source list as a parameter
+        adapter = new Adapter(source);
+
+        // Set Horizontal Layout Manager
+        // for Recycler view
+        HorizontalLayout
+                = new LinearLayoutManager(
+                MainActivity.this,
+                LinearLayoutManager.HORIZONTAL,
+                false);
+        recyclerView.setLayoutManager(HorizontalLayout);
+
+        // Set adapter on recycler view
+        recyclerView.setAdapter(adapter);
+    }
+
+    // Function to add items in RecyclerView.
+    public void AddItemsToRecyclerViewArrayList()
+    {
+        // Adding items to ArrayList
+        source = new ArrayList<>();
+        source.add("gfg");
+        source.add("is");
+        source.add("best");
+        source.add("site");
+        source.add("for");
+        source.add("interview");
+        source.add("preparation");
+    }
+}
