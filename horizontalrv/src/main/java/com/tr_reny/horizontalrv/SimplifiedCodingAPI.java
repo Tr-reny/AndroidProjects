68824b6ec7854960201125305ca2e9b946fef771
Tr-reny <rennykipkoech@gmail.com>
2022-08-06 06:10:40 -0400

    build

---
+++ horizontalrv/src/main/java/com/tr_reny/horizontalrv/SimplifiedCodingAPI.java
@@ -0,0 +1,14 @@
+package com.tr_reny.horizontalrv;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface SimplifiedCodingAPI {
+
+    @GET("marvel")
+    Call<List<Marvel>> getMarvels();
+
+
+}
