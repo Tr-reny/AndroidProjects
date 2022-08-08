ac63a5c450b3fbf76ce21e8cdd264b2d29730743
Tr-reny <rennykipkoech@gmail.com>
2022-08-08 14:26:15 -0400

    fully functional code

--- okhttprequestexample/src/main/java/com/tr_reny/okhttprequestexample/MainActivity.java
+++ okhttprequestexample/src/main/java/com/tr_reny/okhttprequestexample/MainActivity.java
@@ -1,8 +1,19 @@
 package com.tr_reny.okhttprequestexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Created on 8th AUG 2022 time: 21:00 hrs
  * This is a simple OkHttpRequest which request JSON object and displayed it TextView
@@ -13,5 +24,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mTextViewResults = findViewById(R.id.textVewResults);
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://reqres.in/api/users?page=2";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()){
+                     String myResponse = response.body().string();
+
+                     MainActivity.this.runOnUiThread(new Runnable() {
+                         @Override
+                         public void run() {
+                             mTextViewResults.setText(myResponse);
+                         }
+                     });
+                }
+
+            }
+        });
+
     }
 }
