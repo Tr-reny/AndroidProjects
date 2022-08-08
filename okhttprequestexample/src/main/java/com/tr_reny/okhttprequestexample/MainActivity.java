ef1559f37a8ea84ca689116a01ed4de74c0c8427
Tr-reny <rennykipkoech@gmail.com>
2022-08-08 13:59:57 -0400

    added permission

--- okhttprequestexample/src/main/java/com/tr_reny/okhttprequestexample/MainActivity.java
+++ okhttprequestexample/src/main/java/com/tr_reny/okhttprequestexample/MainActivity.java
@@ -3,7 +3,10 @@ package com.tr_reny.okhttprequestexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/**
+ * Created on 8th AUG 2022 time: 21:00 hrs
+ * This is a simple OkHttpRequest which request JSON object and displayed it TextView
+ * */
 public class MainActivity extends AppCompatActivity {
 
     @Override
