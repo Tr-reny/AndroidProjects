8df837c24759b2a3738a78343e2f590df6b86e73
Tr-reny <rennykipkoech@gmail.com>
2022-08-08 05:11:45 -0400

    added articles and RapidApiNews.java interface

---
+++ freenewsapi/src/main/java/com/tr_reny/freenewsapi/Articles.java
@@ -0,0 +1,14 @@
+package com.tr_reny.freenewsapi;
+
+public class Articles {
+    private String title;
+    private String summary;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+}
--- freenewsapi/src/main/java/com/tr_reny/freenewsapi/RapidApiNews.java
+++ freenewsapi/src/main/java/com/tr_reny/freenewsapi/RapidApiNews.java
@@ -1,4 +1,12 @@
 package com.tr_reny.freenewsapi;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface RapidApiNews {
+
+   @GET("v1/search?q=Elon%20Musk&lang=en")
+    Call<List<Articles>> getArticles();
 }
