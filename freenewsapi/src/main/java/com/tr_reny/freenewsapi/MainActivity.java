5b4c31de0ce3864aa51cea3e36f17e0c03d57008
Tr-reny <rennykipkoech@gmail.com>
2022-08-15 04:43:11 -0400

    okhttp appending

--- freenewsapi/src/main/java/com/tr_reny/freenewsapi/MainActivity.java
+++ freenewsapi/src/main/java/com/tr_reny/freenewsapi/MainActivity.java
@@ -13,13 +13,13 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -36,44 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
         textViewResults = findViewById(R.id.TextViewResults);
 
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .addInterceptor(new Interceptor() {
-                    @NonNull
-                    @Override
-                    public okhttp3.Response intercept(@NonNull Chain chain) throws IOException {
-                        Request originalRequest = chain.request();
-
-                        Request newRequest = originalRequest.newBuilder()
-                                .addHeader("X-RapidAPI-Key", "7b17418753msh4f16608e0aa78d7p1a6fe6jsnfc06e90efe18")
-                                .addHeader("X-RapidAPI-Host", "free-news.p.rapidapi.com")
-                                .build();
-
-                        return chain.proceed(newRequest);
-                    }
-                })
-                .addInterceptor(loggingInterceptor)
-                .build();
-
-        String url = "https://free-news.p.rapidapi.com/";
-
-        Retrofit retrofit = new Retrofit.Builder()
-//                .baseUrl("https://www.simplifiedcoding.net/demos/")
-                .baseUrl(url)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(okHttpClient)
-                .build();
-
-        articlesAPi = retrofit.create(ArticlesAPi.class);
-        getArticles();
-
-
-
-
-/*
-
 
         OkHttpClient client = new OkHttpClient();
 
@@ -86,8 +48,37 @@ public class MainActivity extends AppCompatActivity {
                 .addHeader("X-RapidAPI-Host", "free-news.p.rapidapi.com")
                 .build();
 
-
         client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                textViewResults.setText(e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+
+                if (!response.isSuccessful()){
+                    String myResponse = response.body().string();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                JSONObject jsonObject = new JSONObject(myResponse);
+                                textViewResults.setText(jsonObject.getJSONObject("articles").getString("author")+ " " + jsonObject.getJSONObject("articles").getString("total_hits"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+
+ /*       client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
                 e.printStackTrace();
@@ -122,41 +113,9 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             }
-        });
-*/
+        });*/
 
 
-    }
 
-    private void getArticles() {
-        Call<List<Articles>> call = articlesAPi.getArticles();
-
-        call.enqueue(new Callback<List<Articles>>() {
-            @Override
-            public void onResponse(Call<List<Articles>> call, retrofit2.Response<List<Articles>> response) {
-
-                if (!response.isSuccessful()) {
-                    textViewResults.setText("Code: " + response.code());
-                    return;
-                }
-                List<Articles> articles = response.body();
-                for (Articles article : articles) {
-                    String content = "";
-                    content += "Title: " + article.getTitle() + "\n";
-                    content += "Summary: " + article.getSummary() + "\n";
-
-                    textViewResults.append(content);
-                }
-
-
-            }
-
-            @Override
-            public void onFailure(Call<List<Articles>> call, Throwable t) {
-              textViewResults.setText(t.getMessage());
-
-
-            }
-        });
     }
 }
