be0b309737d49bc4c2c61479a1d5d1f28986e258
Tr-reny <rennykipkoech@gmail.com>
2022-08-19 05:54:32 -0400

    asynchronous execution

--- retrofitrvsearchview/src/main/java/com/tr_reny/retrofitrvsearchview/MainActivity.java
+++ retrofitrvsearchview/src/main/java/com/tr_reny/retrofitrvsearchview/MainActivity.java
@@ -3,15 +3,69 @@ package com.tr_reny.retrofitrvsearchview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by Reny Kipkoech on 19th AUG 2022
  * This is an App which shows the functionality of Retrofit with RecycleView and SearchView together
  * */
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewResults = findViewById(R.id.tvResults);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderAPI jsonPlaceHolderAPI = retrofit.create(JsonPlaceHolderAPI.class);
+
+        Call<List<Post>> call = jsonPlaceHolderAPI.getPost();
+        //execute
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()){
+                    textViewResults.setText("CODE: " + response.code());
+                    return;
+                }
+
+                List<Post> posts = response.body();
+                for (Post post : posts){
+                    String content = "";
+
+                    content += "ID: " + post.getId() + "\n";
+                    content += "UserID: " + post.getUserId() + "\n";
+                    content += "Body: " + post.getBody() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+
+                    textViewResults.append(content);
+                }
+
+
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResults.setText(t.getMessage());
+
+            }
+        });
+
     }
 }
