aa0dc1f89c63fdf8c194d63c27f8371d9cf7c5ea
Tr-reny <rennykipkoech@gmail.com>
2022-08-19 05:25:25 -0400

    post class

---
+++ retrofitrvsearchview/src/main/java/com/tr_reny/retrofitrvsearchview/Post.java
@@ -0,0 +1,22 @@
+package com.tr_reny.retrofitrvsearchview;
+
+public class Post {
+    private String title,body;
+    private Integer userId,id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
