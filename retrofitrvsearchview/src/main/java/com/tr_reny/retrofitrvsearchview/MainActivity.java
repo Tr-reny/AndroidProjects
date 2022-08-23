eda9650004b425d4bc6e8bf76e73c3d6889d0962
Tr-reny <rennykipkoech@gmail.com>
2022-08-23 06:33:07 -0400

    removed un used resources

--- retrofitrvsearchview/src/main/java/com/tr_reny/retrofitrvsearchview/MainActivity.java
+++ retrofitrvsearchview/src/main/java/com/tr_reny/retrofitrvsearchview/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +18,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 /**
  * Created by Reny Kipkoech on 19th AUG 2022
  * This is an App which shows the functionality of Retrofit with RecycleView and SearchView together
- * */
+ */
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ArrayList<Post> arrayListPost;
@@ -30,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
         arrayListPost = new ArrayList<>();
-        /* postList = new ArrayList<>();*/
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
@@ -49,12 +49,6 @@ public class MainActivity extends AppCompatActivity {
                 }
                 List<Post> posts = response.body();
                 for (Post post : posts) {
-                  /*  String content = "";
-
-                    content += "ID: " + post.getId() + "\n";
-                    content += "UserID: " + post.getUserId() + "\n";
-                    content += "Body: " + post.getBody() + "\n";
-                    content += "Title: " + post.getTitle() + "\n";*/
 
                     arrayListPost.add(post);
 
@@ -73,74 +67,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void PutDataIntoRecylerView(ArrayList<Post> arrayListPost) {
 
-        MyAdapter myAdapter = new MyAdapter(this,arrayListPost);
+        MyAdapter myAdapter = new MyAdapter(this, arrayListPost);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(myAdapter);
 
     }
-            //execute
-        /*call.enqueue(new Callback<List<Post>>() {
-            @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                if (!response.isSuccessful()){
-                   *//* textViewResults.setText("CODE: " + response.code());*//*
-                    return;
-                }
-
-                List<Post> posts = response.body();
-                for (Post post : posts){
-                   *//* String content = "";
-
-                    content += "ID: " + post.getId() + "\n";
-                    content += "UserID: " + post.getUserId() + "\n";
-                    content += "Body: " + post.getBody() + "\n";
-                    content += "Title: " + post.getTitle() + "\n";*//*
-                   postList.add(post);
-
-                }
-                PutDataIntoRecylerView(postList);
-
-
-            }
-
-            @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
-
-            }
-        });*/
-
-    }
-   /* private void PutDataIntoRecylerView(List<Post> postList) {
-
-        MyAdapter myAdapter = new MyAdapter(this,postList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(myAdapter);
-
-    }
-*/
-    //
-   /* public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu_item,menu);
-
-        MenuItem searchItem = menu.findItem(R.id.menu_search);
-
-        SearchView searchView = null;
-        if (searchItem != null){
-            searchView = (SearchView) searchItem.getActionView();
-        }
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                MyAdapter.getFilter().filter(newText);
-                return true;
-            }
-        });
-        return super.onCreateOptionsMenu(menu);
-
-    }*/
+}
