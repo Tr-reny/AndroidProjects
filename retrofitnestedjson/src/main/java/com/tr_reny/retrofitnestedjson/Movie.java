7f06a5b66426daf9b7eb5d3f56ff7c69320752f1
Tr-reny <rennykipkoech@gmail.com>
2022-08-26 16:02:57 -0400

    created model class

---
+++ retrofitnestedjson/src/main/java/com/tr_reny/retrofitnestedjson/Movie.java
@@ -0,0 +1,32 @@
+package com.tr_reny.retrofitnestedjson;
+
+public class Movie {
+    private String Title ,Year,Genre,Director,Plot,imdbRating;
+
+    //getter
+
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getYear() {
+        return Year;
+    }
+
+    public String getGenre() {
+        return Genre;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public String getPlot() {
+        return Plot;
+    }
+
+    public String getImdbRating() {
+        return imdbRating;
+    }
+}
