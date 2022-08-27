2d0424e50bf94ac537df862463954a81ab0fc15d
Tr-reny <rennykipkoech@gmail.com>
2022-08-27 02:13:24 -0400

    mainActivity update

--- retrofitnestedjson/src/main/java/com/tr_reny/retrofitnestedjson/MainActivity.java
+++ retrofitnestedjson/src/main/java/com/tr_reny/retrofitnestedjson/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("")
+                .addConverterFactory(GsonConverterFactory)
+                .build();
+
     }
 }
