4894757d7a33f3a67a79200a6ae2b9f06da555da
Tr-reny <rennykipkoech@gmail.com>
2022-08-27 02:09:39 -0400

    interface update

--- retrofitnestedjson/src/main/java/com/tr_reny/retrofitnestedjson/movieAPI.java
+++ retrofitnestedjson/src/main/java/com/tr_reny/retrofitnestedjson/movieAPI.java
@@ -1,4 +1,11 @@
 package com.tr_reny.retrofitnestedjson;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface movieAPI {
+    @GET("")
+    Call<List<Movie>> getMovieList();
 }
