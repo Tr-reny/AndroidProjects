1993a75ba851233d50b146a1ccb3ce648f86b09e
Tr-reny <rennykipkoech@gmail.com>
2022-08-27 02:48:13 -0400

    asynchronous run

--- retrofitnestedjson/src/main/java/com/tr_reny/retrofitnestedjson/MainActivity.java
+++ retrofitnestedjson/src/main/java/com/tr_reny/retrofitnestedjson/MainActivity.java
@@ -3,14 +3,18 @@ package com.tr_reny.retrofitnestedjson;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private MovieAPI movieAPI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,26 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        MovieAPI movieAPI = retrofit.create(MovieAPI.class);
+       movieAPI = retrofit.create(MovieAPI.class);
+        getMoveList();
 
+    }
+    private void getMoveList(){
+        Call<List<Movie>> call = movieAPI.getMovieList();
+        call.enqueue(new Callback<List<Movie>>() {
+            @Override
+            public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
+                if (!response.isSuccessful()){
+
+                  return;
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Movie>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 }
