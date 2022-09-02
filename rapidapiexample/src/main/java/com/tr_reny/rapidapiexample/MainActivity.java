1c8c0f0e8763f776cc27d980d709eb55633a4391
Tr-reny <rennykipkoech@gmail.com>
2022-09-02 15:37:53 -0400

    retrofit

--- rapidapiexample/src/main/java/com/tr_reny/rapidapiexample/MainActivity.java
+++ rapidapiexample/src/main/java/com/tr_reny/rapidapiexample/MainActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+/**Created by Reny K. On 2nd Sep 2022
+ * This is a Simple app which GET NewsAPI from RapidAPI and Displays it RecyclerView
+ * */
 
 public class MainActivity extends AppCompatActivity {
+
+    private RapidApi rapidApi;
+    private List<News> newsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://crypto-news-live3.p.rapidapi.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        rapidApi = retrofit.create(RapidApi.class);
+
+        getNews();
 
     }
 }
