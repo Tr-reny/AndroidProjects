96f68f669eb7dfd4bebe4a97c4c45480bb88afa4
Tr-reny <rennykipkoech@gmail.com>
2022-09-02 15:30:54 -0400

    headers

--- rapidapiexample/src/main/java/com/tr_reny/rapidapiexample/RapidApi.java
+++ rapidapiexample/src/main/java/com/tr_reny/rapidapiexample/RapidApi.java
@@ -9,8 +9,11 @@ import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 public interface RapidApi {
+//    'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com'
+    //   'X-RapidAPI-Key': '7b17418753msh4f16608e0aa78d7p1a6fe6jsnfc06e90efe18',
+    // URL 'https://crypto-news-live3.p.rapidapi.com/news',
 
-    @GET("")
+    @GET("news")
     Call<List<News>> getNews( @Header("X-RapidAPI-Host") String api,
                               @Header("X-RapidAPI-Key") String apiKey,
                               @Query("query") String query_str);
