1fd2a94785bf209b86633f1c115ac1118c2d1681
Tr-reny <rennykipkoech@gmail.com>
2022-09-04 05:17:29 -0400

    interface api creation

---
+++ pagingretrofit/src/main/java/com/tr_reny/pagingretrofit/JsonPlaceHolderApi.java
@@ -0,0 +1,14 @@
+package com.tr_reny.pagingretrofit;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+    //We will parse data from this link https://jsonplaceholder.typicode.com/photos
+    //We will separate page by "albumId"
+
+    @GET("photos")
+    Call<List<Photos>> getPhotos();
+}
