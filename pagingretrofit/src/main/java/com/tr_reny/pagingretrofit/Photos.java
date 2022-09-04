e01728d79b8f155f40b21391bf26c273fd28fcac
Tr-reny <rennykipkoech@gmail.com>
2022-09-04 02:28:50 -0400

    getters

---
+++ pagingretrofit/src/main/java/com/tr_reny/pagingretrofit/Photos.java
@@ -0,0 +1,32 @@
+package com.tr_reny.pagingretrofit;
+
+public class Photos {
+    private Integer albumId;
+    private Integer id;
+    private String title;
+    private String url;
+    private String thumbnailUrl;
+
+    //getters
+
+
+    public Integer getAlbumId() {
+        return albumId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+}
