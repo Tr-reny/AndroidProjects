53327f4e21c34dbce5cd9ad8853870fc93a26c26
Tr-reny <rennykipkoech@gmail.com>
2022-09-04 02:19:22 -0400

    readme

--- pagingretrofit/src/main/java/com/tr_reny/pagingretrofit/MainActivity.java
+++ pagingretrofit/src/main/java/com/tr_reny/pagingretrofit/MainActivity.java
@@ -3,8 +3,16 @@ package com.tr_reny.pagingretrofit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+/**
+ * Project Created on 4th Sep 2022 by Reny Kipkoech
+ * --------------------------------------------Project Purpose------------------------------------------------------
+ * Suppose you have 5000 images data from a backend API and you do not want load whole 5000 images data at once
+ * that time you can use the paging library. This only load small amounts of data from your large data set.
+ * It will consume less bandwidth. Also, fewer resources resulting in a smooth app and nice user experience.
+ * */
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
