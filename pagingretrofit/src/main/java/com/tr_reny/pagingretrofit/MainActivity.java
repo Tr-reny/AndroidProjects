6f7ea7a27dfd90fb2b3da7a303398062b5f5fa32
Tr-reny <rennykipkoech@gmail.com>
2022-09-11 12:09:04 -0400

    permission

--- pagingretrofit/src/main/java/com/tr_reny/pagingretrofit/MainActivity.java
+++ pagingretrofit/src/main/java/com/tr_reny/pagingretrofit/MainActivity.java
@@ -1,8 +1,16 @@
 package com.tr_reny.pagingretrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Project Created on 4th Sep 2022 by Reny Kipkoech
  * --------------------------------------------Project Purpose------------------------------------------------------
@@ -13,10 +21,23 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private List<Photos> photosList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+/*        photosList = new ArrayList<>();
+
+
+        //retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        */
     }
 }
