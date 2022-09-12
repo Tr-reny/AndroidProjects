d2d5c38a7e04235e864099bbe80d1d150535a073
Tr-reny <rennykipkoech@gmail.com>
2022-09-12 15:01:14 -0400

    baseViewHolder

---
+++ pagingretrofit/src/main/java/com/tr_reny/pagingretrofit/BaseViewHolder.java
@@ -0,0 +1,26 @@
+package com.tr_reny.pagingretrofit;
+
+/**
+ * BaseViewHolder is a helper class, It responsible for manage multiple view holders in an easier way.
+ * It was written a separate article on Best Practices of RecyclerView in Android
+ * link --> https://androidwave.com/android-recyclerview-example-best-practices/
+ * */
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public abstract class BaseViewHolder extends RecyclerView.ViewHolder {
+    private int mCurrentPosition;
+    public BaseViewHolder(View itemView) {
+        super(itemView);
+    }
+    protected abstract void clear();
+    public void onBind(int position) {
+        mCurrentPosition = position;
+        clear();
+    }
+    public int getCurrentPosition() {
+        return mCurrentPosition;
+    }
+}
