6ee30bb34f6753234bff9f55b0bb46a0ec19af9a
Tr-reny <rennykipkoech@gmail.com>
2022-09-12 15:17:14 -0400

    scrollLessener

---
+++ pagingretrofit/src/main/java/com/tr_reny/pagingretrofit/PaginationListener.java
@@ -0,0 +1,43 @@
+package com.tr_reny.pagingretrofit;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+/**
+ * PaginationScrollListener is utility java class that extends RecyclerView.OnScrollListener.
+ * Now must have define PAGE_SIZE here and some useful methods like
+ * loadMoreItems(), isLastPage() and isLoading() all methods uses explain later
+ * */
+
+public abstract class PaginationListener extends RecyclerView.OnScrollListener {
+    public static final int PAGE_START = 1;
+    @NonNull
+    private LinearLayoutManager layoutManager;
+    /**
+     * Set scrolling threshold here (for now i'm assuming 10 item in one page)
+     */
+    private static final int PAGE_SIZE = 10;
+    /**
+     * Supporting only LinearLayoutManager for now.
+     */
+    public PaginationListener(@NonNull LinearLayoutManager layoutManager) {
+        this.layoutManager = layoutManager;
+    }
+    @Override
+    public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+        int visibleItemCount = layoutManager.getChildCount();
+        int totalItemCount = layoutManager.getItemCount();
+        int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+        if (!isLoading() && !isLastPage()) {
+            if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
+                    && firstVisibleItemPosition >= 0
+                    && totalItemCount >= PAGE_SIZE) {
+                loadMoreItems();
+            }
+        }
+    }
+    protected abstract void loadMoreItems();
+    public abstract boolean isLastPage();
+    public abstract boolean isLoading();
+}
