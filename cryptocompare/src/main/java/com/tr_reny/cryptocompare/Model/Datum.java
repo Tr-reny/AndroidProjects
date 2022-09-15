4506bd04ded9218f72349c5801719e41caeae888
Tr-reny <rennykipkoech@gmail.com>
2022-09-15 02:31:41 -0400

    build and model class

---
+++ cryptocompare/src/main/java/com/tr_reny/cryptocompare/Model/Datum.java
@@ -0,0 +1,21 @@
+package com.tr_reny.cryptocompare.Model;
+
+
+public class Datum {
+
+    public String id;
+    public String guid;
+    public Integer publishedOn;
+    public String imageurl;
+    public String title;
+    public String url;
+    public String source;
+    public String body;
+    public String tags;
+    public String categories;
+    public String upvotes;
+    public String downvotes;
+    public String lang;
+    public SourceInfo sourceInfo;
+
+}
