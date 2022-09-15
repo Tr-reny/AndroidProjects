79cefee6db27b8a034c61c5f712af646dbef5b04
Tr-reny <rennykipkoech@gmail.com>
2022-09-15 02:46:06 -0400

    interface

---
+++ cryptocompare/src/main/java/com/tr_reny/cryptocompare/Interface/cryptocompare.java
@@ -0,0 +1,18 @@
+package com.tr_reny.cryptocompare.Interface;
+
+import com.tr_reny.cryptocompare.Model.News;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
+public interface cryptocompare {
+
+    @GET("data/v2/news/?lang=EN")
+    Call<List<News>> getNews(
+//            @Header("API Host") String apiHost,
+            @Header("API KEY in URL") String apiKey);
+
+}
--- cryptocompare/src/main/java/com/tr_reny/cryptocompare/MainActivity.java
+++ cryptocompare/src/main/java/com/tr_reny/cryptocompare/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    /** Created on 15th Aug 2022 by Reny K.
+     * This is a practise app to test to see if i can Display Api data from
+     * https://min-api.cryptocompare.com/
+     * as Well as Crypto Market Graphs
+     * */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
