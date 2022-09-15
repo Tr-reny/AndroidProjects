d1216860005d7138dda83af329055076cd7dbb46
Tr-reny <rennykipkoech@gmail.com>
2022-09-15 02:52:22 -0400

    MyAdapterClass

---
+++ cryptocompare/src/main/java/com/tr_reny/cryptocompare/Adapter/MyAdapter.java
@@ -0,0 +1,22 @@
+package com.tr_reny.cryptocompare.Adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+
+
+
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
